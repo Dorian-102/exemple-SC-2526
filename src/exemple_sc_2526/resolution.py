c71f456d694e263ce05e307aa8f4655e5a4c38ac
Dorian-102 <petit.dorian12@gmail.com>
2026-03-04 10:51:54 +0100

    rectification fonction lambda

--- src/exemple_sc_2526/resolution.py
+++ src/exemple_sc_2526/resolution.py
@@ -10,12 +10,12 @@ def resolution(probleme: ProblemeTransport) -> SolutionTransport:
     c = np.array(probleme.couts_unitaires)
     n, m = len(probleme.entrepots), len(probleme.clients)
     Aeq = np.fromfunction(
-        lambda (i,k): 1. if (1 <= (k - (i-1) * m) <= m) else 0., 
+        lambda i,k: 1. if (1 <= (k - (i-1) * m) <= m) else 0., 
         shape=(m, m * n)
     )
     beq = np.array(probleme.clients)
     Aub = np.fromfunction(
-        lambda (j,k): 1. if 1 <= (1 + (k - j) // m) <=n else 0., 
+        lambda j,k: 1. if 1 <= (1 + (k - j) // m) <=n else 0., 
         shape=(n, n * m)
     )
     bub = np.array(probleme.entrepots)
