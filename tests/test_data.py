b14ee8ba932c4b7595d33a9924abd823091eb001
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2026-03-04 10:46:55 +0100

    Implémente les tests de vérification pour ProblemeTransport

--- tests/test_data.py
+++ tests/test_data.py
@@ -8,6 +8,21 @@ from pydantic import ValidationError
 from exemple_sc_2526.data import ProblemeTransport, SolutionTransport
 
 
-def test_verifications_probleme_transport():
+def test_verifications_probleme_transport_entrepots():
     with pytest.raises(ValidationError):
-        ...
+        ProblemeTransport(entrepots=[], clients=[1.0], couts_unitaires=[1.0])
+
+
+def test_verifications_probleme_transport_clients():
+    with pytest.raises(ValidationError):
+        ProblemeTransport(entrepots=[1.0], clients=[], couts_unitaires=[1.0])
+
+
+def test_verifications_probleme_transport_couts():
+    with pytest.raises(ValidationError):
+        ProblemeTransport(entrepots=[1.0, 2.0], clients=[1.0], couts_unitaires=[1.0])
+
+
+def test_verifications_probleme_transport_quantites():
+    with pytest.raises(ValidationError):
+        ProblemeTransport(entrepots=[1.0], clients=[2.0], couts_unitaires=[1.0])
